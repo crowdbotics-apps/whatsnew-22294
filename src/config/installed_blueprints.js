13259afe2facab421c8bb9286a0f533ae915840c
Crowdbotics <team@crowdbotics.com>
2020-11-04 15:59:35 +0000

    add package blueprint-react-native-feed-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'Feed24167522', human_name: 'Feed 24', access_route: 'Feed24167522'},
 { name: 'Maps25167521', human_name: 'Maps 25', access_route: 'Maps25167521', icon: 'map'},
 { name: 'Settings167506', human_name: 'Settings', access_route: 'Settings167506'},
 
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import Feed24167522Navigator from '../features/Feed24167522/navigator';
 import Maps25167521Navigator from '../features/Maps25167521/navigator';
 import Settings167506Navigator from '../features/Settings167506/navigator';
 
@@ -15,6 +16,7 @@ import Settings167506Navigator from '../features/Settings167506/navigator';
 const AppNavigator = {
 
     //@BlueprintNavigationInsertion
+Feed24167522: { screen: Feed24167522Navigator },
 Maps25167521: { screen: Maps25167521Navigator },
 Settings167506: { screen: Settings167506Navigator },
 
